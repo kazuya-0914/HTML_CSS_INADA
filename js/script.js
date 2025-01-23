22b2e9c8db36757fa407d5304af1347a2eb56535
kazuya-0914 <kazu.work.0914@gmail.com>
2025-01-23 20:19:20 +0900

    1/23 PM コミット

--- js/script.js
+++ js/script.js
@@ -1,26 +1,10 @@
 import { fetchWeatherData } from "./weather.js";
 import { DeepL } from "./translation.js";
+let html = '';
+const domCode = document.getElementById('dom-code');
 
 // ----- 教材コード
 try {
-
-$(function() {
-
-  $('#fadeOut').on('click', function () {
-    $('.box').css('opacity', 0);
-  });
-  $('#fadeIn').on('click', function () {
-    $('.box').css('opacity', 1);
-  });
-  $('#slideUp').on('click', function () {
-    $('.box').css('height', 0);
-  });
-  $('#slideDown').on('click', function () {
-    $('.box').css('height', 200);
-  });
-
-});
-
 
 } catch(e) {
   const deepLmsg = await DeepL(e.message);
@@ -28,6 +12,8 @@ $(function() {
 }
 
 // -----
+
+domCode.innerHTML = html;
 
 // 天気予報
 // fetchWeatherData();
